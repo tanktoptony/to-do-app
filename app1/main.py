9f93536ae54385013bed667431f5595c394782f6
Anthony Jarina <anthony.jarina@gmail.com>
2024-01-25 17:59:41 -0600

    code declutter figuring in default values

--- app1/main.py
+++ app1/main.py
@@ -1,12 +1,17 @@
-
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """Read a text file and return the list of
+    to-do items."""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(filepath, todos_arg):
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """Write to the original text file with added or
+    completed to do items."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -15,14 +20,14 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos('todos.txt', todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -35,12 +40,12 @@ while True:
             print(number)
             number = number - 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + "\n"
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
         except ValueError:
             print('Your command is not valid.')
             continue
@@ -49,12 +54,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
             index = number - 1
             todos_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todos_to_remove} was removed from the list."
             print(message)
@@ -65,6 +70,5 @@ while True:
         break
     else:
         print("Command is not valid.")
-            
+
 print("Bye!")
-            
