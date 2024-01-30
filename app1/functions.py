b92591cc95ba72e540cb995f942e222e777497e3
tanktoptony <anthony.jarina@gmail.com>
2024-01-30 16:07:08 -0600

    implementing mild modularity features

---
+++ app1/functions.py
@@ -0,0 +1,13 @@
+def get_todos(filepath="todos.txt"):
+    """Read a text file and return the list of
+    to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """Write to the original text file with added or
+    completed to do items."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- app1/main.py
+++ app1/main.py
@@ -1,17 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """Read a text file and return the list of
-    to-do items."""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """Write to the original text file with added or
-    completed to do items."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+#from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -20,14 +8,14 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -40,12 +28,12 @@ while True:
             print(number)
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + "\n"
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print('Your command is not valid.')
             continue
@@ -54,12 +42,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todos_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todos_to_remove} was removed from the list."
             print(message)
