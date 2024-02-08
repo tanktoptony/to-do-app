53442f12a8610ad6216a0112b91dfb22ded9c388
tanktoptony <anthony.jarina@gmail.com>
2024-02-08 16:29:29 -0600

    implemented add button to append todo entries to the txt file

--- app1/gui.py
+++ app1/gui.py
@@ -2,9 +2,23 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Please Enter a To Do")
-input_box = sg.InputText(tooltip="Type a to-do")
+input_box = sg.InputText(tooltip="Type a to-do", key="To-Do")
 add_button = sg.Button('Add')
 
-window = sg.Window('My To Do App', layout=[[label, input_box, add_button]])
-window.read()
+window = sg.Window('My To Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['To-Do'] + "\n"
+            todos.append((new_todo))
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
